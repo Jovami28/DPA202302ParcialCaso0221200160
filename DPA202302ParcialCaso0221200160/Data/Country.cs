fc4a167f69e6291f738f5110937e02e0989dbe86
laboratorioesan <laboratorioesan@WS2302510>
2023-10-14 19:48:09 -0500

    Agregar archivos de proyecto.

---
+++ DPA202302ParcialCaso0221200160/Data/Country.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DPA202302ParcialCaso0221200160.Data;
+
+public partial class Country
+{
+    public string Id { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public string? Code { get; set; }
+
+    public string? Currency { get; set; }
+}
